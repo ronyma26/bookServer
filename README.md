cfa24153ab8f9058cf2672c668970a72da5fe5ff
ronyma26 <138403231+ronyma26@users.noreply.github.com>
2023-07-02 23:54:58 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# bookServer
+serverless book inventory management 
+
+AWS Lambda functions ,API gateway , Dynamo DB , 
+
---
+++ main.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      hashicorp-learn = "lambda-api-gateway"
+    }
+  }
+
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
